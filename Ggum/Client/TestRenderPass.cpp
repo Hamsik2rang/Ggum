c7ce63997a9affa4793cb8d246287d8c09a7be71
Im Yongsik <lvhi7121@gmail.com>
2023-10-06 08:00:56 +0900

    ImGui 색상 변경

--- Ggum/Client/TestRenderPass.cpp
+++ Ggum/Client/TestRenderPass.cpp
@@ -34,6 +34,31 @@ void TestRenderPass::OnRender()
 
 void TestRenderPass::OnGUI()
 {
-	bool showDemoWindow = true;
-	ImGui::ShowDemoWindow(&showDemoWindow);
+	static bool showDemoWindow = true;
+    static ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImGuiIO& io = ImGui::GetIO();
+    if (showDemoWindow)
+    {
+		ImGui::ShowDemoWindow(&showDemoWindow);
+    }
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &showDemoWindow);      // Edit bools storing our window open/close state
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clearColor); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+    }
 }
